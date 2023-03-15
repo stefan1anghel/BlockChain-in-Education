a3b4cc765d7ca341b71938b05e58d52919b69ef7
Stefan <stefan.anghel00@gmail.com>
2023-03-15 17:42:21 +0200

    Wrappere pentru baze de date

--- Actors/education_entity_class.py
+++ Actors/education_entity_class.py
@@ -1,13 +1,16 @@
 from Classes.student_milestone_class import StudentMilestone
+from Utils.common_methods import id_generator
+from database_engine import DbEngine
 
 
 class EducationEntity:
-    def __init__(self, entity_type, entity_name):
-        self.entity_type = entity_type
+    def __init__(self, entity_name):
         self.entity_name = entity_name
-        self.transactions_list = []
+        self.id = id_generator("education_entity")
+        db = DbEngine.instance()
+        db.run_query(f"insert into EducationEntities values({self.id}, '{self.entity_name}')")
 
-    def approve_transaction(self, transaction_index):
-        approved_transaction: StudentMilestone = self.transactions_list[transaction_index]
-        approved_transaction.education_entity_approval = True
-        self.transactions_list.remove(self.transactions_list[transaction_index])
+    # def approve_transaction(self, transaction_index):
+    #     approved_transaction: StudentMilestone = self.transactions_list[transaction_index]
+    #     approved_transaction.education_entity_approval = True
+    #     self.transactions_list.remove(self.transactions_list[transaction_index])
